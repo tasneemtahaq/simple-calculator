8dc49100f77ea77302566f786d20c68dfd08766f
tasneem <tasneemtahaq@gmail.com>
2024-04-19 20:51:54 +0500

    new feature

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const answer = await inquirer.prompt([
         message: "Select one of the operators to perform operation",
         type: "list",
         name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Exponent"],
     },
 ]);
 //conditional statement
@@ -22,6 +22,9 @@ else if (answer.operator === "Multiplication") {
 }
 else if (answer.operator === "Division") {
     console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Exponent") {
+    console.log(answer.firstNumber ** answer.secondNumber);
 }
 else {
     console.log("Please select valid operator");
--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const answer = await inquirer.prompt([
     message: "Select one of the operators to perform operation",
     type: "list",
     name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    choices: ["Addition", "Subtraction", "Multiplication", "Division","Exponent"],
   },
 ]);
 //conditional statement
@@ -22,6 +22,8 @@ if (answer.operator === "Addition") {
   console.log(answer.firstNumber * answer.secondNumber);
 } else if (answer.operator === "Division") {
   console.log(answer.firstNumber / answer.secondNumber);
-} else {
+} else if (answer.operator === "Exponent") {
+  console.log(answer.firstNumber ** answer.secondNumber);}
+else {
   console.log("Please select valid operator");
 }
